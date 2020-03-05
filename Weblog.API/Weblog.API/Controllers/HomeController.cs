ac76f4bcdc0e27e9d4afbe6972696f16c6b6294d
sepehr <sepehr@cerenade.com>
2020-03-05 13:07:24 -0800

    cleaned up project

---
+++ Weblog.API/Weblog.API/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Weblog.API.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+    }
+}
