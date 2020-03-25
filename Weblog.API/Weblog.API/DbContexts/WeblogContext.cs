9a851d8be8475c00a850289f8e32c86bd8d3dfda
Sepehr <Sepehr@DESKTOP-J432CR0>
2020-03-25 09:38:29 -0700

    set up mock data context

--- Weblog.API/Weblog.API/DbContexts/WeblogContext.cs
+++ Weblog.API/Weblog.API/DbContexts/WeblogContext.cs
@@ -18,10 +18,10 @@ namespace Weblog.API.DbContexts
         public WeblogContext()
         { }
 
-        public DbSet<User> Users { get; set; }
+        public virtual DbSet<User> Users { get; set; }
         public virtual DbSet<Blog> Blogs { get; set; }
-        public DbSet<Post> Posts { get; set; }
-        public DbSet<Comment> Comments { get; set; }
+        public virtual DbSet<Post> Posts { get; set; }
+        public virtual DbSet<Comment> Comments { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
